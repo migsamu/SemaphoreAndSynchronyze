0c77141fe4fb63c2826ec2099120153343b2a1c5
Miguel <mufrikiler@gmail.com>
2021-10-06 23:38:45 +0200

    fix semaphore

--- src/main/java/org/iesfm/threadsSafe/semaphore/Main.java
+++ src/main/java/org/iesfm/threadsSafe/semaphore/Main.java
@@ -9,20 +9,27 @@ public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
+    private static final Semaphore semaphore = new Semaphore(1);
+
+    private static final Accumulator acc = new Accumulator();
+
     public static void main(String[] args) {
-
-
-        Semaphore semaphore = new Semaphore(1);
-        Accumulator acc = new Accumulator();
 
         IncrementTask incrementTask = new IncrementTask(semaphore, acc, 100);
         DecrementTask decrementTask = new DecrementTask(semaphore, acc, 100);
-        Thread t = new Thread(incrementTask);
 
+        Thread t = new Thread(incrementTask);
         Thread t2 = new Thread(decrementTask);
+
         t.start();
         t2.start();
 
+        try {
+            t.join();
+            t2.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
         log.info("Incrementa un total de " + acc.getTimes() + " veces");
     }
 }
